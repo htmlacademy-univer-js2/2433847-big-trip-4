6320daaab36be757543a6c4e1004a04568fe8edd
swweaterthief <vaniished@mail.ru>
2024-04-13 19:12:09 +0500

    module 1 task 3

--- src/view/tripView.js
+++ src/view/tripView.js
@@ -1,7 +1,6 @@
 import {createElement} from '../render';
 
-const tripTemplate =
-`<ul class="trip-events__list"></ul>`;
+const tripTemplate = '<ul class="trip-events__list"></ul>';
 
 export default class TripView {
   getTemplate() {
