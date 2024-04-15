bacea9815385455f44a1d51884434287ca004fdd
swweaterthief <vaniished@mail.ru>
2024-04-15 23:56:36 +0500

    module 4 task 2

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import FilterView from './view/filterView';
 import TripInfoView from './view/tripInfoView';
 import Route from './model/route';
 import {render, RenderPosition} from './framework/render';
+import {generateHeaderData} from './mock/headerInfo';
 
 const header = document.querySelector('.trip-main');
 const tripFilters = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
 const route = new Route();
+route.generatePoints(3);
 const filters = [
   {name: 'Everything', checked: true},
   {name: 'Future', checked: false},
@@ -16,7 +18,8 @@ const filters = [
 ];
 const presenter = new TripPresenter(route, filters);
 
-render(new TripInfoView(), header, RenderPosition.AFTERBEGIN);
+const headerData = generateHeaderData();
+render(new TripInfoView(headerData), header, RenderPosition.AFTERBEGIN);
 render(new FilterView(filters), tripFilters);
 
 presenter.init(tripEvents);
--- src/view/tripInfoView.js
+++ src/view/tripInfoView.js
@@ -1,20 +1,25 @@
 import AbstractView from '../framework/view/abstract-view';
 
-const TripInfoTemplate =
+const TripInfoTemplate = (data) =>
   `<section class="trip-main__trip-info trip-info">
     <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+        <h1 class="trip-info__title">${data.title}</h1>
 
-        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+        <p class="trip-info__dates">${data.dates}</p>
     </div>
 
     <p class="trip-info__cost">
-        Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">${data.total}</span>
     </p>
 </section>`;
 
 export default class TripInfoView extends AbstractView {
+  constructor(data) {
+    super();
+    this.data = data;
+  }
+
   get template() {
-    return TripInfoTemplate;
+    return TripInfoTemplate(this.data);
   }
 }
