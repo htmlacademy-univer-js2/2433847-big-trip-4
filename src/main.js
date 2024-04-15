4df0263febb75f29a9a9a59492d3bc4dc64ca939
swweaterthief <vaniished@mail.ru>
2024-04-15 23:56:36 +0500

    module 4 task 2

--- src/main.js
+++ src/main.js
@@ -8,10 +8,15 @@ const header = document.querySelector('.trip-main');
 const tripFilters = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
 const route = new Route();
-route.generatePoints(4);
-const presenter = new TripPresenter(route);
+const filters = [
+  {name: 'Everything', checked: true},
+  {name: 'Future', checked: false},
+  {name: 'Present', checked: false},
+  {name: 'Past', checked: false},
+];
+const presenter = new TripPresenter(route, filters);
 
 render(new TripInfoView(), header, RenderPosition.AFTERBEGIN);
-render(new FilterView(), tripFilters);
+render(new FilterView(filters), tripFilters);
 
 presenter.init(tripEvents);
