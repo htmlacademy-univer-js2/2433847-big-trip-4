26617f75d71b19148ea1381485cea29b3971141c
swweaterthief <vaniished@mail.ru>
2024-05-27 01:20:57 +0500

    module 6 task 2

--- src/template/editFormTemplate.js
+++ src/template/editFormTemplate.js
@@ -85,16 +85,10 @@ export const editFormTemplate = (routePoint) => `<li class="trip-events__item">
 
             <div class="event__field-group  event__field-group--time">
                 <label class="visually-hidden" for="event-start-time-1">From</label>
-                <input class="event__input  event__input--time" id="event-start-time-1" type="text"
-                       name="event-start-time" value="${routePoint.timeFrom.toLocaleString([], {
-    day: 'numeric', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit'
-  })}">
+                <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time">
                 &mdash;
                 <label class="visually-hidden" for="event-end-time-1">To</label>
-                <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time"
-                       value="${routePoint.timeTo.toLocaleString([], {
-    day: 'numeric', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit'
-  })}">
+                <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time">
             </div>
 
             <div class="event__field-group  event__field-group--price">
