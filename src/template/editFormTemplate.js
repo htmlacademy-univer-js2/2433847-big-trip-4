695af545983c39fd2f95d8705b74dbbf82690dc4
swweaterthief <vaniished@mail.ru>
2024-05-27 13:11:08 +0500

    numerical price field

--- src/template/editFormTemplate.js
+++ src/template/editFormTemplate.js
@@ -96,7 +96,7 @@ export const editFormTemplate = (routePoint) => `<li class="trip-events__item">
                     <span class="visually-hidden">Price</span>
                     &euro;
                 </label>
-                <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price"
+                <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price"
                        value="${routePoint.price}">
             </div>
 
